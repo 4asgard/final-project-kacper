4c53eb7ca018bd56625f7b489da6af601b565654
Agnieszka S <mail.sadowska@gmail.com>
2023-01-14 15:35:45 +0100

    Zajęcia z Kacper Warda - 20230114

--- src/main/java/com/wsb/logintegra/thymeleaflayout/ThymeleafLayoutApplication.java
+++ src/main/java/com/wsb/logintegra/thymeleaflayout/ThymeleafLayoutApplication.java
@@ -12,9 +12,9 @@ public class ThymeleafLayoutApplication {
     public static void main(String[] args) {
         SpringApplication.run(ThymeleafLayoutApplication.class, args);
         Person.people = new ArrayList<>();
-        Person.people.add(new Person(1, "kacper", "kacper.warda@logintegra.com", true, "ADMIN"));
-        Person.people.add(new Person(2, "pioter", "pioter.warda@logintegra.com", true, "USER"));
-        Person.people.add(new Person(3, "janusz", "janusz.warda@logintegra.com", true, "USER"));
+        Person.people.add(new Person(1, "agnieszka", "agnieszka@wsb.com", true, "ADMIN"));
+        Person.people.add(new Person(2, "piotr", "piotr@wsb.com", true, "USER"));
+        Person.people.add(new Person(3, "noname", "noname@wsb.com", true, "USER"));
     }
 
 }
